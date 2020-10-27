c80246fb2a97986b99c057b9c44538a2fa7b0634
Sylvain Estevez <sylvain@luxe.com>
2020-10-27 07:47:05 +0100

    :sparkles: Support for TS 4

--- src/utils/join.ts
+++ src/utils/join.ts
@@ -1,4 +1,4 @@
-import { PROTOCOL_REG } from "../constants/protocol-reg";
+import { PROTOCOL_REG } from '../constants/protocol-reg';
 
 /**
  * @description Similar to Node's Path.join(), but for URLs.
@@ -11,9 +11,9 @@ import { PROTOCOL_REG } from "../constants/protocol-reg";
 export function join(...parts: string[]) {
   const args = Array.prototype.slice.call(arguments, 0);
   let ret = args.join('/');
-  let protocol = ret.match(PROTOCOL_REG);
-  protocol = protocol ? protocol[0] : '';
+  const protocolMatches = ret.match(PROTOCOL_REG);
+  const protocol = protocolMatches ? protocolMatches[0] : '';
   ret = ret.replace(protocol, '');
-  if (protocol) ret = ret.replace(/^\/+/, '');
+  if (protocol) { ret = ret.replace(/^\/+/, ''); }
   return protocol + ret.replace(/\/{2,}/g, '/');
 }
